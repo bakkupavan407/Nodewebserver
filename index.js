f235f2cfa6e2f52d963bcf1d3a251d74af8f15a3
unknown <pbakku@PBAKKU.nisum.com>
2015-06-24 23:20:31 +0530

    PK: Pushing changes

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Hey finally deployed successfully');
+  //response.send('Hello World! Hey finally deployed successfully');
+  response.render('public/index.html');
 });
 
 app.listen(app.get('port'), function() {
