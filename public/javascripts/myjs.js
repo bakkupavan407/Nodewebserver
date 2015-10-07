df6d88982f48afe018e157aa05412d2bccaf0a9e
pbakku <pbakku@nisum.com>
2015-10-07 16:46:06 +0530

    Loading images from script using json object

--- public/javascripts/myjs.js
+++ public/javascripts/myjs.js
@@ -1,18 +1,58 @@
-console.log(blueimp);
 
-document.getElementById('links').onclick = function (event) {
+document.getElementById('links').onclick = function(event) {
     event = event || window.event;
     var target = event.target || event.srcElement,
         link = target.src ? target.parentNode : target,
-        options = {index: link, event: event},
+        options = {
+            index: link,
+            event: event
+        },
         links = this.getElementsByTagName('a');
     blueimp.Gallery(links, options);
 };
 
-/*blueimp.Gallery(
-    document.getElementById('links').getElementsByTagName('a'),
-    {
-        container: '#blueimp-gallery-carousel',
-        carousel: true
+var myJsonObj = [{
+    title: "bakkupeta",
+    path: "bkpvinayaka.jpg"
+}, {
+    title: "bakkupeta",
+    path: "bkpyouth.jpg"
+}, {
+    title: "bakkupeta",
+    path: "bkpvinayaka.jpg"
+}, {
+    title: "bakkupeta",
+    path: "bkpyouth.jpg"
+}, {
+    title: "bakkupeta",
+    path: "bkpvinayaka.jpg"
+}];
+
+setTimeout(function() {
+    for (var i = 0; i < myJsonObj.length; i++) {
+        var maincontainer, subcontainer, anchortag, imgtag, loader;
+
+        maincontainer = document.getElementById('links');
+
+        subcontainer = document.createElement('div');
+        subcontainer.className = "col-sm-6 col-md-3";
+
+        anchortag = document.createElement('a');
+        anchortag.href = "images/" + myJsonObj[i].path;
+        anchortag.className = "thumbnail";
+
+        imgtag = document.createElement('img');
+        imgtag.src = "../images/" + myJsonObj[i].path;
+        imgtag.className = "img-responsive";
+
+        anchortag.appendChild(imgtag);
+
+        subcontainer.appendChild(anchortag);
+        maincontainer.appendChild(subcontainer);
+
+        maincontainer.style.display = "block";
+
+        loader = document.getElementById('loader');
+        loader.style.display = "none";
     }
-);*/
+}, 2000);
